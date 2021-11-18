9c4545f07a200b804c281a6af35cfeb7fe130252
martin lowe <martinlowe673@gmail.com>
2021-11-18 07:45:17 +0000

    progress

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Button, Menu, Typography, Avatar } from "antd";
+import { Link } from "react-router-dom";
+import {
+  HomeOutlined,
+  MoneyCollectOutlined,
+  BulbOutlined,
+  FundOutlined,
+  MenuOutlined,
+} from "@ant-design/icons";
+
+const Navbar = () => {
+  return (
+    <div className="nav-container">
+      <div className="logo-cont">
+        <Avatar />
+        <Typography.Tile level={2} className="logo">
+          <Link to="/">Cryptoverse</Link>
+        </Typography.Tile>
+        {/*<Button className="menu-control-container">
+
+  </Button>*/}
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
