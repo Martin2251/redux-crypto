955d3cb48910106dd700e6472120d3faa60f28c3
martin lowe <martinlowe673@gmail.com>
2021-11-20 16:02:24 +0000

    additons and components

---
+++ src/components/Cryptocurriencies.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Cryptocurriencies = () => {
+  return <div>Homepage</div>;
+};
+
+export default Cryptocurriences;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,7 +20,18 @@ const Navbar = () => {
         </Typography.Title>
       </div>
       <Menu theme="dark">
-        <Menu.Item icon={<HomeOutlined />}></Menu.Item>
+        <Menu.Item icon={<HomeOutlined />}>
+          <Link to="/">Home</Link>
+        </Menu.Item>
+        <Menu.Item icon={<FundOutlined />}>
+          <Link to="/cryptocurrencies">CryptoCurrencies</Link>
+        </Menu.Item>
+        <Menu.Item icon={<MoneyCollectOutlined />}>
+          <Link to="/exchanges">Exchanges</Link>
+        </Menu.Item>
+        <Menu.Item icon={<BulbOutlined />}>
+          <Link to="/news">News</Link>
+        </Menu.Item>
       </Menu>
     </div>
   );
