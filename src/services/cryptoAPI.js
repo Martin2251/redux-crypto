e389a4d08fe1bcaa462d7261d09adcb16505e432
martin lowe <martinlowe673@gmail.com>
2021-11-25 08:03:28 +0000

    added headers

--- src/services/cryptoAPI.js
+++ src/services/cryptoAPI.js
@@ -1,9 +1,20 @@
-/*var options = {
-  method: 'GET',
-  url: 'https://coinranking1.p.rapidapi.com/stats',
-  headers: {
-    'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    'x-rapidapi-key': 'e0db8e15edmshd2ee394bb7399e8p11f48fjsn41b4ece282b9'
-  }
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query";
+
+const cryptoApiHeaders = {
+  "x-rapidapi-host": "coinranking1.p.rapidapi.com",
+  "x-rapidapi-key": "e0db8e15edmshd2ee394bb7399e8p11f48fjsn41b4ece282b9",
 };
-*/
+
+const baseUrl = "https://coinranking1.p.rapidapi.com";
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/exchanges"),
+    }),
+  }),
+});
